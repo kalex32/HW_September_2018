ee1b2c921a2aa65f9934a142195e635d19a2badf
Olexiy Koval <kalex32@ukr.net>
2018-09-10 18:37:05 +0300

    Создание массива экземляров разработчиков

--- src/DZ2_Collections/RunDev.java
+++ src/DZ2_Collections/RunDev.java
@@ -9,14 +9,20 @@ public class RunDev {
         String name = scannerRunDev.nextLine();
         System.out.println("Команда из скольки человек?");
         int capacity = scannerRunDev.nextInt();
-        Team team = new Team(name,capacity);
-        System.out.println("Введите имя:");
+        Team team = new Team(name, capacity);
+        String[] dev = new String[capacity];
+        for (int i = 0; i < capacity; i++) {
+            System.out.println("Введите имя:");
+            dev[i]=scannerRunDev.next();
+//            Developer developer = "developer".concat(String.valueOf(i));
+            Developer dev[i] = new Developer(scannerRunDev.next());
+
+        }
 
 //        Developer developer1 = new Developer(scannerRunDev.next(), true, true,
 //                false,true, true,
 //                false,true,true,2);
 
-        Developer developer2=new Developer(scannerRunDev.next());
         System.out.println(developer2.toString());
 
     }
