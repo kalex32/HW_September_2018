3a9fa9102343334d854138704035b68925c55a7a
Olexiy Koval <kalex32@ukr.net>
2018-09-21 12:05:55 +0300

    Добавляем цену и сортировку по цене

--- src/DZ3_Collections/Toyota.java
+++ src/DZ3_Collections/Toyota.java
@@ -6,7 +6,7 @@ class Toyota {
    static ArrayList<Toyota> cars = new ArrayList<>();
 
     Toyota() {
-        cars.add(new Avensis());
+        cars.add(new Corolla());
         cars.add(new Rav4());
         cars.add(new Camry());
     }
