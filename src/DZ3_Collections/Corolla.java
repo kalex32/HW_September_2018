4cbf4bd57fe89d33a2986b306dab1fe9683c3597
Olexiy Koval <kalex32@ukr.net>
2018-09-21 12:09:08 +0300

    Добавляем цены и сортировку по цене

---
+++ src/DZ3_Collections/Corolla.java
@@ -0,0 +1,24 @@
+package DZ3_Collections;
+
+class Corolla extends Toyota {
+    private String name;
+    private int price;
+
+    Corolla() {
+        this.name = "Corolla";
+        this.price = 459_270;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Toyota " + getName() + " цена - " + getPrice() + " грн.";
+    }
+}
