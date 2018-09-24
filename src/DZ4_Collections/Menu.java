e8b70dbb2a9fa290ebf1010d1b4d02bcaba5b750
Olexiy Koval <kalex32@ukr.net>
2018-09-25 00:02:21 +0300

    Строим меню на базе Мясоедовской, 7 каждое блюдо - отдельный класс

---
+++ src/DZ4_Collections/Menu.java
@@ -0,0 +1,20 @@
+package DZ4_Collections;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+class Menu {
+    void mainMenu(){
+        Scanner scannerMainMenu = new Scanner(System.in);
+
+        System.out.println("Буквально впиться зубами в сочный мясной стейк и забыть обо всем на свете. \n" +
+                "И в первую очередь – о голоде, утолить который можно только с качественно приготовленными блюдами. \n" +
+                "Именно такие подают в нашем ресторане, который приглашает всех ценителей вкусной еды европейской кухни!");
+        System.out.println();
+        try
+        {
+            System.out.println(1.   );
+        }
+        catch (InputMismatchException e){
+    }
+}
