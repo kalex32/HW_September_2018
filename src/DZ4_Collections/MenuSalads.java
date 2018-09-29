2c53f85ddcc1abd9d7791bba65350214d55b1596
Olexiy Koval <kalex32@ukr.net>
2018-09-30 00:12:12 +0300

    создаем экземпляры салатов

--- src/DZ4_Collections/MenuSalads.java
+++ src/DZ4_Collections/MenuSalads.java
@@ -2,8 +2,9 @@ package DZ4_Collections;
 
 class MenuSalads {
     static void menuSalads() {
+        Salads saladCezar = new SaladCezar();
         System.out.println("Выбираем салат: \n" +
-                "1. " + SaladCezar.getNameSalad() + "\n" +
-                "(курица, яйцо перепелиное, салат айсберг, томаты, сыр пармезан)");
+                "1. " + saladCezar.getNameSalad() + "\n" /*+
+                "(курица, яйцо перепелиное, салат айсберг, томаты, сыр пармезан)"*/);
     }
 }
---
+++ src/DZ4_Collections/runMenu.java
@@ -0,0 +1,8 @@
+package DZ4_Collections;
+
+public class runMenu {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.mainMenu();
+    }
+}
